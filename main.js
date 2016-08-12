e5c5dcaf1f66324ff1e4b130e1107a08558fee6f
jakexia72 <jakexia72@hotmail.com>
2016-08-12 00:23:07 -0400

    text will now clear after "Sent"

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ function sendMail() {
     var link = "mailto:me@example.com" + "?cc=myCCaddress@example.com" + "&subject=" + escape(document.getElementById('mySubject').value) + "&body=" + escape(document.getElementById('myText').value);
 
     window.location.href = link;
+
+    document.getElementById("myText").value = "";
+    document.getElementById("mySubject").value = "";
 }
