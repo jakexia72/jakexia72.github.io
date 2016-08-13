0d696c286425e72c8be07a851312e9fbdfc0a87e
jakexia72 <jakexia72@hotmail.com>
2016-08-13 00:26:40 -0400

    ADDED FAQ TO CONTACT PAGE!!!!

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-var main = function () {
-    $('#toggle').click(function () {
-        $('#showme').fadeToggle();
-    });
-}
-
-$(document).ready(main);
-
 function sendMail() {
     var link = "mailto:me@example.com" + "?cc=myCCaddress@example.com" + "&subject=" + escape(document.getElementById('mySubject').value) + "&body=" + escape(document.getElementById('myText').value);
 
