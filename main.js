9a8bbbe1429d065ecbd4acbcf50dc911213f2ca8
jakexia72 <jakexia72@hotmail.com>
2019-01-14 19:24:56 -0500

    UPDATE

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ var main = function () {
     }
 
 $('#fullsizeDiv1').height($('#row1').height());
-    $('#fullsizeDiv2').height($('#row2').height());
+    //$('#fullsizeDiv2').height($('#row2').height());
      $('#fullsizeDiv3').height($('#row3').height());
 $('#fullsizeDiv6').height($('#row6').height());
     $('#fullsizeDiv4').height($('#row4').height());
