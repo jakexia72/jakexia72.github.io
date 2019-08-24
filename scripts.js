938995582ead9eef520e608c6e4fcc27910dd10f
jakexia72 <jakexia72@hotmail.com>
2019-08-24 00:47:10 -0400

    began redesign

---
+++ scripts.js
@@ -0,0 +1,12 @@
+//
+// $(document).ready(function(){
+//
+// });
+// $(document).scroll(function() {
+//   if($('#test1')[0].getBoundingClientRect().bottom >= $('#newSection')[0].getBoundingClientRect().top ){
+//     console.log($('#test1').position().top );
+//
+//     $('#test1').css('position','absolute');
+//     $('#test1').css('top',(window.pageYOffset + $('#test1').position().top) +'px');
+//   }
+// });
